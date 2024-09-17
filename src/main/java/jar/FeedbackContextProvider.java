340caa85bef108886eb710c10928862c87f70f4f
shreeharshg <shreeharshg65@gmail.com>
2024-09-17 11:41:24 +0530

    Added close, search, toggle view functionality

--- src/main/java/jar/FeedbackContextProvider.java
+++ src/main/java/jar/FeedbackContextProvider.java
@@ -17,8 +17,8 @@ public class FeedbackContextProvider extends AbstractJiraContextProvider {
 
         // Simulated feedback list with more items
         List<String> feedbackList = new ArrayList<>();
-        for (int i = 1; i <= 20; i++) {
-            feedbackList.add(" This is feedback number " + i git commit -m);
+        for (int i = 1; i <= 10; i++) {
+            feedbackList.add(" This is feedback number " + i + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
         }
 
         contextMap.put("feedbackList", feedbackList);
